0169924c12c22212597424c6850b8586e2f3f63e
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-03-27 05:38:34 +0530

    feat: Implement update task functionality and enhance task routes

--- server/controllers/task.controller.js
+++ server/controllers/task.controller.js
@@ -4,7 +4,6 @@ import { ApiError } from "../utils/ApiError.js";
 import { Task } from "../model/task.model.js";
 
 //! create new task
-// TODO: mongodb aggregations pipeline to get user info like avatar, name,email
 const createTask = AsyncHandler(async (req, res) => {
   const { title, description, project, status, priority, completed } = req.body;
 
@@ -51,9 +50,10 @@ const getTask = AsyncHandler(async (req, res) => {
     .status(200)
     .json(new ApiResponse(200, "Task fetch successfully", task));
 });
-//! get all tasks
-const getAllTask = AsyncHandler(async (req, res) => {
 
+//! get all tasks
+// TODO: mongodb aggregations pipeline to get user info like avatar, name,email
+const getAllTask = AsyncHandler(async (req, res) => {
   const tasks = await Task.find();
   if (!tasks) {
     throw new ApiError(404, "Tasks not found");
@@ -61,13 +61,40 @@ const getAllTask = AsyncHandler(async (req, res) => {
 
   return res
     .status(200)
-    .json(new ApiResponse(200, "Task fetch successfully", tasks));
+    .json(new ApiResponse(200, "All Tasks fetch successfully", tasks));
 });
 //! update task
+const updateTask = AsyncHandler(async (req, res) => {
+  const { taskId } = req.params;
+  if (!taskId) {
+    throw new ApiError(400, "Task Id missing or invalid");
+  }
 
+  const taskExists = await Task.findById(taskId);
+  if (!taskExists) {
+    throw new ApiError(404, "task not found!");
+  }
+
+  if (taskExists.userId.toString() !== req?.user?._id) {
+    throw new ApiError(403, "You are not authorized to access this task");
+  }
+
+  const updateTask = await Task.findByIdAndUpdate(
+    taskExists._id,
+    {
+      ...req.body,
+    },
+    { new: true }
+  );
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, "Task successfully created", updateTask));
+});
 //! delete task
+
 //! compted task  task
 //! status update task
 //! priority update task
 
-export { createTask, getTask,getAllTask };
+export { createTask, getTask, getAllTask, updateTask };
--- server/routers/task.routes.js
+++ server/routers/task.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import {createTask, getAllTask, getTask} from "../controllers/task.controller.js"
+import {createTask, getAllTask, getTask, updateTask} from "../controllers/task.controller.js"
 import {AuthCheck} from "../middlewares/auth.middleware.js"
 
 export const TaskRouter =  Router();
@@ -7,3 +7,5 @@ export const TaskRouter =  Router();
 TaskRouter.post("/",AuthCheck,createTask);
 TaskRouter.get("/",AuthCheck,getAllTask);
 TaskRouter.get("/:taskId",AuthCheck,getTask);
+TaskRouter.patch("/:taskId",AuthCheck,updateTask);
+TaskRouter.delete("/:taskId",AuthCheck,);
