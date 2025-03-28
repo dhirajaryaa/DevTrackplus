81c6568f7a92521d9b7dc97fc1c25546c6693171
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-03-28 20:43:49 +0530

    feat: Enhance avatar upload component with preview and click-to-upload functionality

--- client/src/components/custom/user/AvatarUpload.jsx
+++ client/src/components/custom/user/AvatarUpload.jsx
@@ -1,35 +1,59 @@
-import React, { useState } from "react";
-import { CircleX, Loader2, Upload } from "lucide-react";
+import React, { useRef, useState } from "react";
+import { Camera, CircleX, Loader2, Upload } from "lucide-react";
 import { useSelector } from "react-redux";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 
 function AvatarUpload() {
-  const [isLoading, setIsLoading] = useState();
+  const [isLoading, setIsLoading] = useState(false);
   const { user } = useSelector((state) => state.auth);
+  const [avatarPreview, setAvatarPreview] = useState(user?.avatar?.url || null);
+
+  const avatarRef = useRef(null);
+
+  const handleAvatarClick = () => {
+    if (avatarRef.current) {
+      avatarRef.current.click();
+    }
+  };
+
+  const handleFileChange = (e) => {   
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setAvatarPreview(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
 
   return (
     <div className="w-full">
-      <Avatar className={"size-30 mx-auto relative"}>
-        <AvatarImage src={user?.avatar?.url} alt={user?.name} />
+      <input
+        type="file"
+        accept="image/*"
+        ref={avatarRef}
+        className="hidden"
+        onChange={handleFileChange}
+      />
+      <Avatar
+        className="size-35 mx-auto cursor-pointer relative"
+        onClick={handleAvatarClick}
+      >
+        <div className="absolute size-35 top-0 left-0 bg-transparent hover:bg-gray-900/40 border-accent-foreground/80 flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity">
+          <Camera className="text-white size-8 dark:text-black border-accent-foreground/80 " />
+        </div>
+        <AvatarImage src={avatarPreview} alt={user?.name} />
       </Avatar>
       <div className="w-full flex items-center mt-4 gap-3 justify-center">
-        <div>
-          <input type="file" accept="image/*" />
-        </div>
-        <Button
-          variant="outline"
+        <Button 
           type="button"
-          size="icon"
-          disabled={isLoading}
-        >
-          <CircleX />
-        </Button>
-        <Button
-          type="button"
-          disabled={isLoading}
+          disabled={isLoading }
           variant="outline"
-          size="icon"
+          className={`${
+            avatarPreview === user?.avatar?.url ? "hidden" : "block"
+          }`}
         >
           {isLoading ? <Loader2 className="animate-spin" /> : <Upload />}
         </Button>
