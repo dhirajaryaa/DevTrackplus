a06465b9deae06e978290835a19cf807db43b1b3
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-03-29 05:17:17 +0530

    feat: Implement aggregation for fetching tasks with user details

--- server/controllers/task.controller.js
+++ server/controllers/task.controller.js
@@ -2,6 +2,7 @@ import { AsyncHandler } from "../utils/AsyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
 import { Task } from "../model/task.model.js";
+import mongoose from "mongoose";
 
 //! create new task
 const createTask = AsyncHandler(async (req, res) => {
@@ -54,7 +55,39 @@ const getTask = AsyncHandler(async (req, res) => {
 //! get all tasks
 // TODO: mongodb aggregations pipeline to get user info like avatar, name,email
 const getAllTask = AsyncHandler(async (req, res) => {
-  const tasks = await Task.find();
+  const tasks = await Task.aggregate([
+    {
+      $match: { userId: new mongoose.Types.ObjectId(req.user._id) },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "userId",
+        foreignField: "_id",
+        as: "user_detailed",
+      },
+    },
+    {
+      $addFields: {
+        user_detailed: { $arrayElemAt: ["$user_detailed", 0] },
+      },
+    },
+    {
+      $project: {
+        _id: 1,
+        title: 1,
+        description: 1,
+        status: 1,
+        dueDate: 1,
+        priority: 1,
+        createdAt: 1,
+        "user_detailed.email": 1,
+        "user_detailed.name": 1,
+        "user_detailed.avatar": 1,
+      },
+    },
+  ]);
+
   if (!tasks) {
     throw new ApiError(404, "Tasks not found");
   }
@@ -202,13 +235,15 @@ const updateTaskPriority = AsyncHandler(async (req, res) => {
   const updateTask = await Task.findByIdAndUpdate(
     task?._id,
     {
-      priority
+      priority,
     },
     { new: true }
   );
   return res
     .status(200)
-    .json(new ApiResponse(200, "Task Priority update successfully", updateTask));
+    .json(
+      new ApiResponse(200, "Task Priority update successfully", updateTask)
+    );
 });
 
 export {
@@ -219,5 +254,5 @@ export {
   deleteTask,
   updateTaskStatus,
   toggleTaskCompleted,
-  updateTaskPriority
+  updateTaskPriority,
 };
