e40f164cc8ff6085dc9ca14ba02f1c6a6bc04ddb
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-03-28 05:32:45 +0530

    feat: Implement token refresh mechanism in API queries

--- client/src/app/api/index.js
+++ client/src/app/api/index.js
@@ -1,19 +1,41 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { removeUser } from "../auth/authReducer";
 
+const baseQuery = fetchBaseQuery({
+  baseUrl: import.meta.env.VITE_BASE_URL,
+  credentials: "include",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
-
+export const baseQueryReauth = async (args, api, extraOptions) => {
+  let result = await baseQuery(args, api, extraOptions);
+  if (result?.error?.status === 500) {
+    const newToken = await baseQuery(
+      {
+        url: "users/refresh-token",
+        method: "POST",
+      },
+      api,
+      extraOptions
+    );
+    if (newToken?.data) {
+      // Retry the original request with the new token
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      console.log("❌ Refresh token failed. Logging out...");
+      api.dispatch(removeUser());
+    }
+  }
+  return result;
+};
 
 export const ApiQuery = createApi({
-  baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_BASE_URL,
-    credentials: "include",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  }),
+  baseQuery: baseQueryReauth,
   endpoints: (builder) => ({
     getExample: builder.query({
-      query: () => '',
+      query: () => "",
     }),
   }),
 });
